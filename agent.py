ec364d27172ec379d26ca0cf89962c61ebc6bd1c
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-12-14 15:19:40 +0000

    q learning added

--- agent.py
+++ agent.py
@@ -22,11 +22,10 @@ print("Upper Bound for Env Observation", env.observation_space.high)
 print("Lower Bound for Env Observation", env.observation_space.low)
 print("The action space: {}".format(env.action_space.n))
 
-def discretesize(s):
+def discretize(s):
     s_adjusted = (s - env.observation_space.low)*np.array([10, 100])
     return np.round(s_adjusted, 0).astype(int)
 
-    
 
 def randomAgent():
     return env.action_space.sample()
@@ -47,27 +46,57 @@ N = np.zeros(((num_states[0], num_states[1], env.action_space.n)))
 s,a,r = None, None, None
 terminal = False
 
-#def QLearningAgent(current_state, current_reward):
-obs = env.reset()
-a = env.action_space.sample()
-obs, reward, done, truncated, info = env.step(a)
+alpha, gamma = 0.2, 0.9
+episodes = 1
 
-print(obs)
+epsilon = 0.8
+min_epsilon = 0
+global reduction 
+reduction = (epsilon - min_epsilon)/episodes
 
-print(discretesize(obs))
+def epsilonGreedy(s):
+    global epsilon
+    if np.random.random() < 1-epsilon:
+         action = np.argmax(Q[s[0], s[1]])
+    else:
+        action = np.random.randint(0, env.action_space.n)
+    
+    if epsilon>min_epsilon:
+        epsilon-=reduction
+        
+    return action
 
-print()
 
-print()
+def QLearningAgent(current_state, current_reward):
+    global s,a,r
+    # might need to go outside
+    current_state = discretize(current_state)
+    previous_state = discretize(s)
+    if not terminal:
+        Q[previous_state[0], previous_state[1], a] = current_reward
+    if previous_state!=None:
+        N[previous_state[0], previous_state[1], a]+= 1
+        # update Q table 
+        delta = alpha*(r + (gamma* np.max(Q[current_state[0], current_state[1]])) - Q[previous_state[0], previous_state[1],a])
+        Q[previous_state[0], previous_state[1], a] += delta
+    
+    action = epsilonGreedy(previous_state)
+    previous_state = current_state
+    r = current_reward
+    
+    return action
 
-episodes = 0
-obs = env.reset()
+    
+
+obs = env.reset()[0]
+reward = 0
 done = False
 for step in range(episodes):
     while not done:
-        action = randomAgent()
+        action = QLearningAgent(obs, reward)
+        print(action)
         obs, reward, done, truncated, info = env.step(action)
-        env.render()
+        #env.render()
         time.sleep(0.001)
         
         # If the epsiode is up, then start another one
